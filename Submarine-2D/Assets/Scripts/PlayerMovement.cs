26f42930edba0013e908b555a5c243ed25bcc1f5
Juan Daniel Rubio Camacho <30873813+JDanielRC@users.noreply.github.com>
2020-12-08 23:47:26 -0600

    joystick movement

---
+++ Submarine-2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Joystick joystick;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(joystick.Horizontal, joystick.Vertical, 0) * Time.deltaTime * 5);
+     
+    }
+}
