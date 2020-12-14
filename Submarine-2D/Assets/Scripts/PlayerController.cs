057896585b28acc2cfc7cd94a7e08428994a204e
Juan Pablo Velazco Velasquez <juanpa.jpvv@gmail.com>
2020-12-13 18:40:55 -0600

    GUI Bonita 2.0
    Ora si estos si son los cambios

---
+++ Submarine-2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    public Joystick joystick;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(joystick.Horizontal, joystick.Vertical, 0) * Time.deltaTime * 5);
+        
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+}
+
+    private void OnCollisionEnter2D(Collision2D other){
+        print("Le pegastes wey");
+        if (other.gameObject.tag == "Enemy")
+        {
+            //Destroy(other.gameObject);
+            TakeDamage(10);
+            print("Vida: "+currentHealth);
+        }
+    }
+}
+
---
+++ Submarine-2D/Assets/Scripts/WeponButtonHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeponButtonHandler : MonoBehaviour
+{
+    public void ChangeWepon()
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        print("Arma cambiada a: "+ txt.text); 
+    }
+}
