d2645ececc175c382cb54244320a309400d9c712
Juan Daniel Rubio Camacho <30873813+JDanielRC@users.noreply.github.com>
2020-12-14 15:18:50 -0600

    Movement with screen touch

--- Submarine-2D/Assets/Scripts/PlayerController.cs
+++ Submarine-2D/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 
     public int maxHealth = 100;
     public int currentHealth;
+    public int speed = 5;
 
     public HealthBar healthBar;
     public Joystick joystick;
@@ -21,8 +22,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(joystick.Horizontal, joystick.Vertical, 0) * Time.deltaTime * 5);
+        //JoyStickMovement();
+        ClickMovement();
         
+    }
+
+    void ClickMovement()
+    {
+        // ejecutar únicamente si existe un touch del usuario en la pantalla
+        if (Input.touchCount > 0)
+        {
+            
+            Camera cam = Camera.main;
+            // obtenemos la dirección del punto donde tocó el usuario en torno a la cámara
+            Vector2 direction = cam.ScreenToWorldPoint(Input.GetTouch(0).position);
+            // para no sobrecargar de movimiento/animación, si ya estamos prácticamente encima del punto no se realizará movimiento alguno
+            if (Vector3.Distance(direction, transform.position) > 0.2f)
+            {
+                //caso contrario de que estemos alejados, nos movemos en X & Y hacia ese punto
+                transform.position = Vector2.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+            }
+            
+        }
+        
+
+    }
+
+    void JoyStickMovement()
+    {
+        // Movimiento en X & Y según el input detectado en el joystick
+        transform.Translate(new Vector3(joystick.Horizontal, joystick.Vertical, 0) * Time.deltaTime * 5);
     }
 
     void TakeDamage(int damage)
